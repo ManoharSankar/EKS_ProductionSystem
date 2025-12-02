c608b1e1a82a54c2e8459ed63ae3eaee9616fea9
ManoharSankar <manoharsankar93@gmail.com>
2025-12-02 18:36:03 +0530

    changed the group name

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,24 @@
 output "cluster_name" {
-description = "EKS Cluster Name"
-value       = module.eks.cluster_name
+  description = "EKS Cluster Name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-description = "EKS Cluster Endpoint"
-value       = module.eks.cluster_endpoint
+  description = "EKS Cluster Endpoint"
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_ca_certificate" {
-description = "Cluster CA Certificate"
-value       = module.eks.cluster_certificate_authority_data
+  description = "Cluster CA Certificate"
+  value       = module.eks.cluster_certificate_authority_data
 }
 
 output "managed_node_group_name" {
-description = "EKS Managed Node Group Name"
-value       = module.eks_managed_node_group.node_group_name
+  description = "EKS Managed Node Group Name"
+  value       = keys(module.eks.managed_node_groups)[0]
 }
 
 output "managed_node_group_arn" {
-description = "EKS Managed Node Group ARN"
-value       = module.eks_managed_node_group.arn
-}
-
-output "managed_node_group_id" {
-description = "EKS Managed Node Group ID"
-value       = module.eks_managed_node_group.id
+  description = "EKS Managed Node Group ARN"
+  value       = module.eks.managed_node_groups_arns["eks-managed-ng"]
 }
