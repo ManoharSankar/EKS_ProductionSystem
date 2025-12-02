801201feafc1b2db7f29b89adb750a718d974d92
ManoharSankar <manoharsankar93@gmail.com>
2025-12-02 13:35:23 +0530

    changed the output

--- outputs.tf
+++ outputs.tf
@@ -9,5 +9,6 @@ value = module.eks.cluster_endpoint
 
 
 output "node_group_arn" {
-  value = module.eks.eks_managed_node_groups["worker_ng"].resources.node_group_arn
+  value = module.eks.eks_managed_node_groups["worker_ng"].arn
 }
+
